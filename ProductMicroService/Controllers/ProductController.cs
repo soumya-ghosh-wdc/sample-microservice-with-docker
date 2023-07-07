47decd6a26421848172472fb576820ae89144fc2
Soumya Ghosh <131948372+soumya-ghosh-wdc@users.noreply.github.com>
2023-07-07 16:36:55 +0530

    Add files via upload
    First commit

---
+++ ProductMicroService/Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductMicroService.Repository;
+using ProductMicroService.Models;
+using System.Transactions;
+
+namespace ProductMicroService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var products = _productRepository.GetProducts();
+            return new OkObjectResult(products);
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var product = _productRepository.GetProductByID(id);
+            return new OkObjectResult(product);
+        }
+
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+            using (var scope = new TransactionScope())
+            {
+                _productRepository.InsertProduct(product);
+                scope.Complete();
+                return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+            }
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut]
+        public IActionResult Put([FromBody] Product product)
+        {
+            if (product != null)
+            {
+                using (var scope = new TransactionScope())
+                {
+                    _productRepository.UpdateProduct(product);
+                    scope.Complete();
+                    return new OkResult();
+                }
+            }
+            return new NoContentResult();
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _productRepository.DeleteProduct(id);
+            return new OkResult();
+        }
+    }
+}
